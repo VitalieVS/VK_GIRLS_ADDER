0ea28305cc04fb1e8911da93be63bc037f24640b
VitalieVSV <dut66AHJCT>
2021-02-22 18:15:00 +0200

    gg

---
+++ src/main/java/CreateList.java
@@ -0,0 +1,64 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+
+public class CreateList {
+
+    private ArrayList<Long> createList(String data) {
+        ArrayList<Long> resultList = new ArrayList<>();
+        if (!data.isEmpty()) {
+            JSONObject obj = new JSONObject(data);
+            JSONArray keys = obj.getJSONObject("response").getJSONArray("items");
+
+            for (int i = 0; i < keys.length(); i++) {
+                JSONObject id = keys.getJSONObject(i);
+                resultList.add(id.getLong("id"));
+            }
+        }
+        return resultList;
+    }
+
+    private ArrayList<Long> checkList(ArrayList<Long> list) {
+        ArrayList<Long> resultList = new ArrayList<>();
+        StringBuilder parsedContent = new StringBuilder();
+        URL url;
+
+        for (Long value: list) {
+            try {
+                url = new URL("");
+            } catch (IOException e) {
+                System.out.println("Error Unknonw: " + e);
+            }
+        }
+
+
+        return resultList;
+    }
+
+    private static String getData(String ACCESS_TOKEN) {
+        StringBuilder content = new StringBuilder();
+        //953 - Balti
+        //country id = 15
+       // final String ACCESS_TOKEN = "5e54954f0482784f8299d866c2e1307e12a435afebe0f6a948314815491c71f9e38136b62438983c7a694";
+
+        try {
+            URL url = new URL("https://api.vk.com/method/users.search?count=150&city=953&country=15&age_from=14&age_to=21&sex=1&access_token=" + ACCESS_TOKEN + "&v=5.130");
+            URLConnection urlConn = url.openConnection();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("Error" + e);
+        }
+        return content.toString();
+    }
+}
